cdf90ce9b0bf02953f1c56b32557b3d649e8a08a
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-04-08 14:12:23 -0300

    feat(app): implement app structure with bindings

--- mobilidade_urbana_app/lib/core/bindings/app_bindings.dart
+++ mobilidade_urbana_app/lib/core/bindings/app_bindings.dart
@@ -1,9 +1,36 @@
-// import 'package:get/get.dart';
-// import 'package:mobilidade_urbana_app/features/onboarding/controllers/onboarding_controller.dart';
+// app_bindings.dart
 
-// class AppBindings extends Bindings {
-//   @override
-//   void dependencies() {
-//     Get.lazyPut<OnBoardingController>(() => OnBoardingController());
-//   }
-// }
+import 'package:get/get.dart';
+import 'package:mobilidade_urbana_app/features/onboarding/data/datasources/onboarding_local_datasource.dart';
+import 'package:mobilidade_urbana_app/features/onboarding/data/datasources/onboarding_remote_datasource.dart';
+import 'package:mobilidade_urbana_app/features/onboarding/data/repositories/onboarding_repository.dart';
+import 'package:mobilidade_urbana_app/features/onboarding/presentation/controllers/onboarding_controller.dart';
+
+class AppBindings extends Bindings {
+  @override
+  void dependencies() {
+
+    // 1. Datasources
+    Get.lazyPut<OnboardingLocalDatasource>(
+      () => OnboardingLocalDatasource(),
+    );
+    Get.lazyPut<OnboardingRemoteDatasource>(
+      () => OnboardingRemoteDatasource(),
+    );
+
+    // 2. Repository 
+    Get.lazyPut<OnboardingRepository>(
+      () => OnboardingRepository(
+        Get.find<OnboardingLocalDatasource>(),
+        Get.find<OnboardingRemoteDatasource>(),
+      ),
+    );
+
+    // 3. Controller 
+    Get.lazyPut<OnBoardingController>(
+      () => OnBoardingController(
+        Get.find<OnboardingRepository>(),
+      ),
+    );
+  }
+}
--- mobilidade_urbana_app/lib/main.dart
+++ mobilidade_urbana_app/lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mobilidade_urbana_app/utils/local_storage/hive_init.dart';
+import 'app.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  await HiveInit.init(); 
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(const App());
 }
