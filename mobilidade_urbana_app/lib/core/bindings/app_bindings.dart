be4ed280e8687050ecdbf96cd3a7a57581938aaf
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-04-08 14:07:09 -0300

    feat(onboarding): implement onboarding flow and bindings

--- mobilidade_urbana_app/lib/core/bindings/app_bindings.dart
+++ mobilidade_urbana_app/lib/core/bindings/app_bindings.dart
@@ -1,9 +1,35 @@
-// import 'package:get/get.dart';
-// import 'package:mobilidade_urbana_app/features/onboarding/controllers/onboarding_controller.dart';
+// app_bindings.dart
 
-// class AppBindings extends Bindings {
-//   @override
-//   void dependencies() {
-//     Get.lazyPut<OnBoardingController>(() => OnBoardingController());
-//   }
-// }
+import 'package:get/get.dart';
+import 'package:mobilidade_urbana_app/features/onboarding/data/datasources/onboarding_local_datasource.dart';
+import 'package:mobilidade_urbana_app/features/onboarding/data/datasources/onboarding_remote_datasource.dart';
+import 'package:mobilidade_urbana_app/features/onboarding/data/repositories/onboarding_repository.dart';
+import 'package:mobilidade_urbana_app/features/onboarding/presentation/controllers/onboarding_controller.dart';
+
+class AppBindings extends Bindings {
+  @override
+  void dependencies() {
+    // Datasources
+    Get.lazyPut<OnboardingLocalDatasource>(
+      () => OnboardingLocalDatasource(),
+    );
+    Get.lazyPut<OnboardingRemoteDatasource>(
+      () => OnboardingRemoteDatasource(),
+    );
+
+    // Repository 
+    Get.lazyPut<OnboardingRepository>(
+      () => OnboardingRepository(
+        Get.find<OnboardingLocalDatasource>(),
+        Get.find<OnboardingRemoteDatasource>(),
+      ),
+    );
+
+    // Controller 
+    Get.lazyPut<OnBoardingController>(
+      () => OnBoardingController(
+        Get.find<OnboardingRepository>(),
+      ),
+    );
+  }
+}
