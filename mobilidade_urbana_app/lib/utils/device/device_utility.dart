1e839809cb3099be7e0ca21d878726a0db224941
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-04-08 12:42:51 -0300

    feat(device): implement device registration and token management

---
+++ mobilidade_urbana_app/lib/utils/device/device_utility.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TDeviceUtils {
+
+    static double getBottomNavigationBarHeight() {
+      return kBottomNavigationBarHeight;
+    }
+
+    static double getAppBarHeight() {
+      return kToolbarHeight;
+    }
+}
--- mobilidade_urbana_app/lib/utils/theme/theme.dart
+++ mobilidade_urbana_app/lib/utils/theme/theme.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mobilidade_urbana_app/utils/theme/custom_themes/text_theme.dart';
-import 'package:mobilidade_urbana_app/utils/theme/controllers/theme.controller.dart';
 
 class TAppTheme {
   TAppTheme._();
