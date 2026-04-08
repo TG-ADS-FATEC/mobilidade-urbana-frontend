3aa65506a7de5d6b3bc6f23d4614afd9a9c3b52c
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-04-08 12:32:22 -0300

    feat(bindings): add AppBindings for onboarding controller

---
+++ mobilidade_urbana_app/lib/core/bindings/app_bindings.dart
@@ -0,0 +1,9 @@
+// import 'package:get/get.dart';
+// import 'package:mobilidade_urbana_app/features/onboarding/controllers/onboarding_controller.dart';
+
+// class AppBindings extends Bindings {
+//   @override
+//   void dependencies() {
+//     Get.lazyPut<OnBoardingController>(() => OnBoardingController());
+//   }
+// }
