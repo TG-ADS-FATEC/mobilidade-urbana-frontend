1721cff02b31b2deedfef16c7986f8cddd89e242
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-04-21 13:28:27 -0300

    feat(device-utility): add methods to retrieve screen width and height

--- mobilidade_urbana_app/lib/utils/device/device_utility.dart
+++ mobilidade_urbana_app/lib/utils/device/device_utility.dart
@@ -9,4 +9,12 @@ class TDeviceUtils {
     static double getAppBarHeight() {
       return kToolbarHeight;
     }
+
+    static double getScreenWidth(BuildContext context) {
+      return MediaQuery.of(context).size.width;
+    }
+
+    static double getScreenHeight(BuildContext context) {
+      return MediaQuery.of(context).size.height;
+    }
 }
