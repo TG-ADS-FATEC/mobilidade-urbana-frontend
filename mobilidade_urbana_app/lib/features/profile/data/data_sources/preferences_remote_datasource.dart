0c42738bb5bca9f434c636c53dffa15e145e5e6f
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-04-25 16:40:41 -0300

    feat(preferences): add preferences remote datasource

--- mobilidade_urbana_app/lib/features/profile/data/data_sources/preferences_remote_datasource.dart
+++ mobilidade_urbana_app/lib/features/profile/data/data_sources/preferences_remote_datasource.dart
@@ -1,10 +1,43 @@
 
 
+import 'package:dio/dio.dart';
+import 'package:mobilidade_urbana_app/core/network/dio_client.dart';
 import 'package:mobilidade_urbana_app/features/profile/data/models/preferences_model.dart';
 
 abstract class PreferencesRemoteDatasource {
   Future<PreferencesModel> getPreferences();
-  Future<PreferencesModel> updatePreferences();
+  Future<PreferencesModel> savePreferences(Map<String, dynamic> data);
+  Future<PreferencesModel> updatePreferences(Map<String, dynamic> data);
   Future<void> deletePreferences();
+}
+
+
+class PreferencesRemoteDatasourceImpl implements PreferencesRemoteDatasource {
+  final Dio _dio = DioClient.instance;
+
+  @override
+  Future<PreferencesModel> getPreferences() async {
+    final response = await _dio.get('/preferences');
+    return PreferencesModel.fromJson(response.data);
+  }
+
+  @override
+  Future<PreferencesModel> updatePreferences(Map<String, dynamic> data) async {
+    final response = await _dio.put('/preferences', data: data);
+    return PreferencesModel.fromJson(response.data);
+
+  }
+
+  @override
+  Future<PreferencesModel> savePreferences(Map<String, dynamic> data) async {
+    final response = await _dio.post('/preferences', data: data);
+    return PreferencesModel.fromJson(response.data);
+  }
+
+  @override
+  Future<void> deletePreferences() async {
+    await _dio.delete('/preferences');
+  }
+
 
 }
