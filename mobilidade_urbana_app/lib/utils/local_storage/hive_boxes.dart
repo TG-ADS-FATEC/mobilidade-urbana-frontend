c0d0c591d53f0745762d3ca20dd4cfaeceda63f6
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-04-27 17:11:50 -0300

    chore(hive): comment hive files

--- mobilidade_urbana_app/lib/utils/local_storage/hive_boxes.dart
+++ mobilidade_urbana_app/lib/utils/local_storage/hive_boxes.dart
@@ -1,7 +1,7 @@
 
 
 // Para centralizar os nomes das boxes — evita strings espalhadas
-class HiveBoxes {
-  static const onboarding = 'onboarding';
-
-}
+// class HiveBoxes {
+//   static const onboarding = 'onboarding';
+//
+// }
