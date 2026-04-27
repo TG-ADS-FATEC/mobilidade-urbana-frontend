9efb93da1b646b2c97f11062d6bc9a1e5535af7d
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-04-27 17:13:52 -0300

    Update transport_selector.dart

--- mobilidade_urbana_app/lib/features/onboarding/presentation/widgets/transport_selector.dart
+++ mobilidade_urbana_app/lib/features/onboarding/presentation/widgets/transport_selector.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobilidade_urbana_app/features/onboarding/presentation/controllers/onboarding_controller.dart';
+import 'package:mobilidade_urbana_app/features/profile/domain/entities/preferences.entity.dart';
 import 'package:mobilidade_urbana_app/utils/constants/colors.dart';
 
 class OnboardingTransportSelector extends StatelessWidget {
@@ -12,38 +13,37 @@ class OnboardingTransportSelector extends StatelessWidget {
 
     final transports = [
       (
-        label: 'Ônibus',
-        icon: Icons.directions_bus_outlined,
+      label: 'Ônibus',
+      icon: Icons.directions_bus_outlined,
+      value: TransportType.bus,
       ),
       (
-        label: 'Trem',
-        icon: Icons.train_outlined,
+      label: 'Trem',
+      icon: Icons.train_outlined,
+      value: TransportType.train,
       ),
       (
-        label: 'Metrô',
-        icon: Icons.subway_outlined,
+      label: 'Metrô',
+      icon: Icons.subway_outlined,
+      value: TransportType.subway,
       ),
     ];
 
     return Obx(
-      () =>
-        Column(
-          children: transports.map((transport) {
-            return SwitchListTile(
-              value: controller.isTransportEnabled(transport.label),
-              onChanged: (value) {
-                controller.toggleTransport(transport.label, value);
-              },
-              secondary: Icon(transport.icon),
-              title: Text(transport.label),
-              contentPadding: EdgeInsets.zero,
-              activeColor: TColors.white,
-              activeTrackColor: TColors.primary,
-              inactiveThumbColor: Theme.of(context).colorScheme.outline,
-              inactiveTrackColor: Colors.transparent,
-            );
-          }
-        ).toList(),
+          () => Column(
+        children: transports.map((transport) {
+          return SwitchListTile(
+            value: controller.isTransportSelected(transport.value),
+            onChanged: (_) => controller.toggleTransport(transport.value),
+            secondary: Icon(transport.icon),
+            title: Text(transport.label),
+            contentPadding: EdgeInsets.zero,
+            activeColor: TColors.white,
+            activeTrackColor: TColors.primary,
+            inactiveThumbColor: Theme.of(context).colorScheme.outline,
+            inactiveTrackColor: Colors.transparent,
+          );
+        }).toList(),
       ),
     );
   }
