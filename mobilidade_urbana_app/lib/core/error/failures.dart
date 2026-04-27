e0f1c34606521db83eb2386778e16bc3ae21565d
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-04-27 17:11:00 -0300

    Update failures.dart

--- mobilidade_urbana_app/lib/core/error/failures.dart
+++ mobilidade_urbana_app/lib/core/error/failures.dart
@@ -10,3 +10,15 @@ final class ServerFailure extends AppFailure {
 final class NetworkFailure extends AppFailure {
   const NetworkFailure([super.message = 'Sem conexão']);
 }
+
+final class CacheFailure extends AppFailure {
+  const CacheFailure([super.message = 'Erro ao acessar dados locais']);
+}
+
+final class ValidationFailure extends AppFailure {
+  const ValidationFailure([super.message = 'Dados inválidos']);
+}
+
+final class UnknownFailure extends AppFailure {
+  const UnknownFailure([super.message = 'Ocorreu um erro desconhecido']);
+}
