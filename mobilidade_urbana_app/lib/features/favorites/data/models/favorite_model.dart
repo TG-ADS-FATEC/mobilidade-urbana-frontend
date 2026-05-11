3a59f1d33cd1905da39fedad6de19414767e903b
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-05-11 12:09:41 -0300

    feat(favorite):  add favorite model

---
+++ mobilidade_urbana_app/lib/features/favorites/data/models/favorite_model.dart
@@ -0,0 +1,36 @@
+
+import 'package:mobilidade_urbana_app/features/favorites/domain/entities/favorite_entity.dart';
+
+class FavoriteModel extends FavoriteEntity {
+
+  const FavoriteModel({
+    super.favoriteId,
+    required super.favoriteName,
+    super.address,
+  });
+
+  factory FavoriteModel.fromEntity(FavoriteEntity entity) {
+    return FavoriteModel(
+      favoriteId: entity.favoriteId,
+      favoriteName: entity.favoriteName,
+      address: entity.address,
+    );
+  }
+
+  factory FavoriteModel.fromJson(Map<String, dynamic> json) {
+    return FavoriteModel(
+      favoriteId: json['favoriteId']?.toString(),
+      favoriteName: json['favoriteName'] as String,
+      address: json['address'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'favoriteId': favoriteId,
+    'favoriteName': favoriteName,
+    'address': address,
+  };
+
+
+
+}
