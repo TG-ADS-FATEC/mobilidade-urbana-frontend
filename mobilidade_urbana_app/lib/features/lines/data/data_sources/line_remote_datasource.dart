b3997e9099ce1c801d6cda5376c7cdf866bd5a7e
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-06-06 19:59:21 -0300

    Update line_remote_datasource.dart

--- mobilidade_urbana_app/lib/features/lines/data/data_sources/line_remote_datasource.dart
+++ mobilidade_urbana_app/lib/features/lines/data/data_sources/line_remote_datasource.dart
@@ -1,30 +1,37 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:mobilidade_urbana_app/core/network/dio_client.dart';
 import 'package:mobilidade_urbana_app/features/lines/data/models/line_model.dart';
 
 abstract class LineRemoteDatasource {
-  Future<List<LineModel>> getLines();
+  Future<({List<LineModel> items, bool hasNext})> getLines({int page = 0, int size = 20});
   Future<List<LineModel>> searchLines(String query);
 }
 
 class LineRemoteDataSourceImpl implements LineRemoteDatasource {
   final Dio _dio = DioClient.instance;
 
-  List<dynamic> _extractList(dynamic data, [String key = 'routes']) {
+  List<dynamic> _extractList(dynamic data, [String key = 'items']) {
+    debugPrint('[_extractList] data runtimeType: ${data.runtimeType}');
     if (data is List) return data;
     if (data is Map<String, dynamic>) {
-      final value = data[key] ?? data['data'];
+      debugPrint('[_extractList] keys: ${data.keys.toList()}');
+      final value = data[key] ?? data['routes'] ?? data['data'];
+      debugPrint('[_extractList] value for key "$key": ${value?.runtimeType} | is List: ${value is List}');
       if (value is List) return value;
     }
     throw FormatException('Unexpected response format: $data');
   }
 
   @override
-  Future<List<LineModel>> getLines() async {
-    final response = await _dio.get('/routes');
-    return _extractList(response.data)
+  Future<({List<LineModel> items, bool hasNext})> getLines({int page = 0, int size = 20}) async {
+    final response = await _dio.get('/routes', queryParameters: {'page': page, 'size': size});
+    final data = response.data as Map<String, dynamic>;
+    final items = _extractList(data)
         .map((json) => LineModel.fromJson(json as Map<String, dynamic>))
         .toList();
+    final hasNext = data['hasNext'] as bool? ?? false;
+    return (items: items, hasNext: hasNext);
   }
 
   @override
