436fab7b5dfbf27dd9e5248ecd7776b788272044
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-06-06 18:50:25 -0300

    Create get_lines_usecase.dart

---
+++ mobilidade_urbana_app/lib/features/lines/domain/usecases/get_lines_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:mobilidade_urbana_app/core/data_state/data_state.dart';
+import 'package:mobilidade_urbana_app/features/lines/domain/entities/line_entity.dart';
+import 'package:mobilidade_urbana_app/features/lines/domain/repository/line_repository.dart';
+
+class GetLinesUsecase {
+  final LineRepository repository;
+
+  const GetLinesUsecase(this.repository);
+
+  Future<DataState<List<LineEntity>>> call() async {
+    return await repository.getLines();
+  }
+}
