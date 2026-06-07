f098eb4329b0a55ae11ce7ce4ee7f5c566dea4a3
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-06-07 10:52:14 -0300

    Update service_locator.dart

--- mobilidade_urbana_app/lib/core/di/service_locator.dart
+++ mobilidade_urbana_app/lib/core/di/service_locator.dart
@@ -5,6 +5,8 @@ import 'package:mobilidade_urbana_app/features/lines/data/data_sources/line_remo
 import 'package:mobilidade_urbana_app/features/lines/data/repository/line_repository_impl.dart';
 import 'package:mobilidade_urbana_app/features/lines/domain/repository/line_repository.dart';
 import 'package:mobilidade_urbana_app/features/lines/domain/usecases/get_lines_usecase.dart';
+import 'package:mobilidade_urbana_app/features/lines/domain/usecases/get_metro_lines_usecase.dart';
+import 'package:mobilidade_urbana_app/features/lines/domain/usecases/get_train_lines_usecase.dart';
 import 'package:mobilidade_urbana_app/features/lines/domain/usecases/search_lines_usecase.dart';
 import 'package:mobilidade_urbana_app/features/favorites/data/repository/favorite_repository_impl.dart';
 import 'package:mobilidade_urbana_app/features/favorites/domain/repository/favorite_repository.dart';
@@ -75,10 +77,16 @@ Future<void> setupServiceLocator() async {
     () => LineLocalDatasourceImpl(),
   );
   sl.registerLazySingleton<LineRepository>(
-    () => LineRepositoryImpl(sl()),
+    () => LineRepositoryImpl(sl(), sl()),
   );
   sl.registerLazySingleton<GetLinesUsecase>(
     () => GetLinesUsecase(sl()),
+  );
+  sl.registerLazySingleton<GetMetroLinesUsecase>(
+    () => GetMetroLinesUsecase(sl()),
+  );
+  sl.registerLazySingleton<GetTrainLinesUsecase>(
+    () => GetTrainLinesUsecase(sl()),
   );
   sl.registerLazySingleton<SearchLinesUsecase>(
     () => SearchLinesUsecase(sl()),
